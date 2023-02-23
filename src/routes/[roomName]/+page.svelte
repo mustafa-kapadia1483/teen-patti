551cd9f18e73c2043b0ddf8088c4dc0270dd9e6c
Mustafa Kapadia <mustafakapadia76@gmail.com>
2023-02-23 18:03:12 +0530

    socket disconnect prevent

--- src/routes/[roomName]/+page.svelte
+++ src/routes/[roomName]/+page.svelte
@@ -58,6 +58,14 @@
 		$socket.on('roomData', (res) => {
 			roomData = { ...roomData, ...res };
 		});
+
+		$socket.on('disconnect', (reason) => {
+			if (reason === 'io server disconnect') {
+				// the disconnection was initiated by the server, you need to reconnect manually
+				socket.connect();
+			}
+			// else the socket will automatically try to reconnect
+		});
 	});
 
 	function startGameHandler() {
