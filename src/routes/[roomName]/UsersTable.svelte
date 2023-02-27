66a3e1643abc24711390007f0d10438a103b5b60
Mustafa Kapadia <mustafakapadia76@gmail.com>
2023-02-27 09:55:21 +0530

    extracted users table in separate component

---
+++ src/routes/[roomName]/UsersTable.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let table;
+	export let usersList;
+</script>
+
+<div class="overflow-x-auto">
+	<h2>Users Table:</h2>
+	<table class="table">
+		<!-- head -->
+		<thead>
+			<tr>
+				<th />
+				<th>Username</th>
+				<th>Balance</th>
+				<th>Gain / Lost</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each usersList as user, i}
+				<tr>
+					<td>{i + 1}</td>
+					<td>{user.username}</td>
+					<td>{user.balance}</td>
+					<td>{user.balance - table}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
